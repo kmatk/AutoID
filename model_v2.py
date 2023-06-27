611fee2cd35036f9eb8b2d75bbe84c5ced47d691
Kevin Atkinson <kmatk620@gmail.com>
2023-06-26 23:56:22 -0400

    initial creation of v2

---
+++ model_v2.py
@@ -0,0 +1,2 @@
+# For this model we're going to attempt to treat the age as if it were a regression problem, rather than classification
+# This may help to increase the accuracy of the age estimation +/- a few years.
